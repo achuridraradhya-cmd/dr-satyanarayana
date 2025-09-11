e266dd13ebcfae4d7f383518c0fb6fd7d8ce6b91
achuridraradhya-cmd <achuridraradhya@gmail.com>
2025-09-11 17:33:20 +0530

    Update CTABanner.tsx

--- src/components/CTABanner.tsx
+++ src/components/CTABanner.tsx
@@ -4,12 +4,12 @@ import React from 'react'
 
 export default function CTABanner() {
     return (
-        <Section>
+        <!--Section>
             <Wrapper>
                 <div className='relative w-full'>
                     <Image src='/images/contact/contact-bg-4.svg' width={1200} height={300} className='w-full cursor-pointer' alt='' />
                 </div>
             </Wrapper>
-        </Section>
+        </Section-->
     )
 }
